a41a244a5d1c623489c22ec1023ae07ca0caeab9
pacter-69 <aldomcorb@gmail.com>
2025-07-01 15:24:58 +0200

    añadiendo carpetas básicas
    wwasdaragahael

---
+++ Assets/Scripts/Systems/BasicUtilites.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public interface BasicUtilites
+{
+   // Here there'll be various methods that will be shared among all clases which will help to optimize the logic and programming time.
+}
