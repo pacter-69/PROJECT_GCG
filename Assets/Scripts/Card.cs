0fb63d4ffad3cba93b1c96c415b4aa7c7cddbc6f
pacter-69 <aldomcorb@gmail.com>
2025-07-01 19:02:44 +0200

    Clase virtual de carta
    Cartandonga oi no vamo a comer cartandonga un roguelike con deck buildeao

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public abstract class Card : MonoBehaviour, BasicUtilites
+{
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
